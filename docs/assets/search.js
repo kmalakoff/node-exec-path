76089cac3e3429e501d2cf653a72be504977884e
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-10 22:08:30 -0800

    Upgrade modules

--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,1 +1,3 @@
-window.searchData = JSON.parse("{\"kinds\":{\"1024\":\"Property\"},\"rows\":[{\"kind\":1024,\"name\":\"satisfiesSemverSync\",\"url\":\"modules.html#satisfiesSemverSync\",\"classes\":\"tsd-kind-property\"}],\"index\":{\"version\":\"2.3.9\",\"fields\":[\"name\",\"comment\"],\"fieldVectors\":[[\"name/0\",[0,2.877]],[\"comment/0\",[]]],\"invertedIndex\":[[\"satisfiessemversync\",{\"_index\":0,\"name\":{\"0\":{}},\"comment\":{}}]],\"pipeline\":[]}}");
+window.searchData = JSON.parse(
+  '{"kinds":{"1024":"Property"},"rows":[{"kind":1024,"name":"satisfiesSemverSync","url":"modules.html#satisfiesSemverSync","classes":"tsd-kind-property"}],"index":{"version":"2.3.9","fields":["name","comment"],"fieldVectors":[["name/0",[0,2.877]],["comment/0",[]]],"invertedIndex":[["satisfiessemversync",{"_index":0,"name":{"0":{}},"comment":{}}]],"pipeline":[]}}',
+);
