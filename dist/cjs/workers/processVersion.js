7b4972f9cd4ffa6cb0ae61b6d7b5bc04b69b348e
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-29 08:52:39 -0800

    Upgrade modules

--- dist/cjs/workers/processVersion.js
+++ dist/cjs/workers/processVersion.js
@@ -2,4 +2,4 @@
 module.exports = function processVersion() {
     return process.version;
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { module.exports = exports.default; for (var key in exports) module.exports[key] = exports[key]; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
