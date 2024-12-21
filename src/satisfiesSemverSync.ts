a2f14ef10dc7036e4468968b467b7022580c0a3a
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-21 11:54:48 -0800

    Upgrade modules

--- src/satisfiesSemverSync.ts
+++ src/satisfiesSemverSync.ts
@@ -1,14 +1,14 @@
 import './polyfills.ts';
 
 import path from 'path';
+import url from 'url';
 import envPathKey from 'env-path-key';
 import semver from 'semver';
 
-// @ts-ignore
-import constants from './constants.ts';
-// @ts-ignore
-import existsSync from './existsSync.ts';
+import constants from './constants.js';
+import existsSync from './existsSync.js';
 
+const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 const processVersion = path.join(__dirname, 'workers', 'processVersion.js');
 
 export type satisfiesSemverSyncOptions = {
@@ -19,7 +19,7 @@ let functionExec = null; // break dependencies
 export default function satisfiesSemverSync(versionString: string, options: satisfiesSemverSyncOptions = {}): string | null {
   if (!functionExec) functionExec = require('function-exec-sync'); // break dependencies
 
-  const env = options.env ?? process.env;
+  const env = options.env || process.env;
   const pathKey = envPathKey(env);
   const envPaths = env[pathKey].split(path.delimiter);
 
