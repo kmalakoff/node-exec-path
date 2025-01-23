448deb46c6c6671b5bca2df22424c5b415ccfbce
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-23 08:56:33 -0800

    Upgrade modules

--- src/polyfills.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-
-if (!path.delimiter) {
-  const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
-  // @ts-ignore
-  path.delimiter = isWindows ? ';' : ':';
-}
