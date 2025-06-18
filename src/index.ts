a359bebe87e2a05df1b014b071a725b8ae20cac4
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-17 23:41:48 -0700

    Update Biome configuration

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { default as satisfiesSemverSync } from './satisfiesSemverSync.js';
 export type { satisfiesSemverSyncOptions } from './satisfiesSemverSync.js';
+export { default as satisfiesSemverSync } from './satisfiesSemverSync.js';
